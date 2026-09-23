1e36797d33c03810dbd3df6a6a492e7c72f96b26
sergiuangheltraian <sergiuangheltraian@gmail.com>
2026-09-23 16:02:28 +0200

    Add files via upload

---
+++ hola_mundo.py
@@ -0,0 +1,18 @@
+#print("Hola mundo")
+#a = "pitón"
+#print(type(a))
+#a = 44
+#print(a)
+#print(type(a))
+#a,b,c = 1,2,3
+#print(a,b,c)
+#a = input()
+#print(a)
+#nombre = input("introduce tu nombre\n")
+#print(nombre)
+diccionario = {
+    "nombre" : "Sergiu",
+    "apellido" : "Anghel",
+    "edad" : "19"
+}
+print (diccionario)
